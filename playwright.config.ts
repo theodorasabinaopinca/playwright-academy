73bd44b989027ed23123a0f14cd7e548a63a0867
Theodora Opinca <theodora.opinca@netcentric.biz>
2026-07-22 15:27:41 +0300

    Added custom scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('')`. */
-		baseURL: "http://google.com",
+		baseURL: process.env.BASE_URL || "http://google.com",
 
 		headless: false,
 
