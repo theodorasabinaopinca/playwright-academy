9ce3587294c470382b2a595a40517a0b152df1e5
Theodora Opinca <theodora.opinca@netcentric.biz>
2026-07-30 10:38:07 +0300

    Running tests only on chrome project

--- tests/bad-test.spec.ts
+++ tests/bad-test.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from "@playwright/test";
 test("login test", async ({ page }) => {
 	await page.goto("https://example.com/login");
 
-	// L Bad: CSS selector instead of getByRole
+	// Bad: CSS selector instead of getByRole
 	await page.click("#loginBtn");
 
-	// L Bad: Hardcoded wait
+	// Bad: Hardcoded wait
 	await page.waitForTimeout(3000);
 
-	// L Bad: Non-retrying assertion for UI state
+	// Bad: Non-retrying assertion for UI state
 	expect(page.url()).toBe("https://example.com/dashboard");
 });
